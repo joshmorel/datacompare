489aa476cef234dfff15725a40764d8fefa6d140
Josh Morel <morel.josh@hotmail.com>
2016-02-24 08:19:37 -0500

    Update DataComp_example.py

--- DataComp_example.py
+++ DataComp_example.py
@@ -1,9 +1,12 @@
 import os
-os.chdir("C:/Users/Josh.Josh-PC/Documents/Beginning Software Engineering/MyProjectDataCompare/4Development")
-os.getcwd()
+os.chdir("C:/Users/Josh.Josh-PC/Documents/Beginning Software Engineering/MyProjectDataCompare")
 import DataCompare as dc
+import pandas as pd
+import imp
+imp.reload(dc)
 
 SQLs = dc.get_file_paths("C:/Users/Josh.Josh-PC/Documents/Beginning Software Engineering/MyProjectDataCompare/4Development/SQL/")
+SQLtest = dc.get_file_paths("C:/Users/Josh.Josh-PC/Documents/Beginning Software Engineering/MyProjectDataCompare/tests/")
 working_cnxn_path = "C:/Users/Josh.Josh-PC/Documents/Beginning Software Engineering/MyProjectDataCompare/4Development/cnxn.txt" 
 
 ## Small data set
@@ -18,8 +21,8 @@ data_to_inspect = dc1.compare_data()
 
 
 ## duplicate data set
-dc1 = dc.DataComp(cnxn_path = working_cnxn_path,left_cnxn_name = "GDELT",left_script_path = SQLs["GDELTfact_CA"],datetofrom=('2015-09-29','2015-09-30'))
-dc1.add_right_data("GDELT",SQLs["GDELTfact_CA_duplicate"])          
+dc1 = dc.DataComp(cnxn_path = working_cnxn_path,left_cnxn_name = "GDELT",left_script_path = SQLtest["test_load_pkduplicate"],datetofrom=('2015-09-29','2015-09-30'))
+dc1.add_right_data("GDELT",SQLtest["test_normal"])
 data_to_inspect = dc1.compare_data()
 
 
@@ -34,6 +37,31 @@ data_to_inspect = dc1.compare_data()
 dce = dc.DataComp(cnxn_path = working_cnxn_path,left_cnxn_name = "GDELT",left_script_path = SQLs["GDELTfact_CA"],datetofrom=('2015-09-29','2015-09-30'))
 dce.add_right_data("GDELT",SQLs["GDELTfact_CA"])
 data_to_inspect = dce.compare_data()
+
+
+
+
+## Right from dataframe 
+dce = dc.DataComp(cnxn_path = working_cnxn_path,left_cnxn_name = "GDELT",left_script_path = SQLs["GDELTfact_CA"],datetofrom=('2015-09-29','2015-09-30'))
+mydf = pd.DataFrame({"a":[1,2],"b":["a","b"],"NumArticles":[1,100]})
+dce.add_right_data(right_cnxn_name = None,right_script_path = None,source = "DataFrame",DataFrame = mydf)
+data_to_inspect = dce.compare_data()
+
+
+
+## No common cols
+dce = dc.DataComp(cnxn_path = working_cnxn_path,left_cnxn_name = "GDELT",left_script_path = SQLtest["test_compare_left_noshared"],datetofrom=('2015-09-29','2015-09-30'))
+mydf = pd.DataFrame({"a":[1,2],"b":["a","b"],"NumArticles":[1,100]})
+dce.add_right_data(right_cnxn_name = None,right_script_path = None,source = "DataFrame",DataFrame = mydf)
+data_to_inspect = dce.compare_data()
+
+
+## Diff rows
+
+dce = dc.DataComp(cnxn_path = working_cnxn_path,left_cnxn_name = "GDELT",left_script_path = SQLtest["test_compare_diffrow_left"],datetofrom=('2015-09-29','2015-09-30'))
+dce.add_right_data("GDELT",SQLtest["test_compare_diffrow_right"])
+data_to_inspect = dce.compare_data()
+
 
 
 
